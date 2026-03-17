2bf060e9d95f204ee89ced701cc4ed9394b7a478
Vladimyr23 <vladimyr.yes@gmail.com>
2026-03-17 11:25:41 +0000

    lessons 15-21

--- Ravesli_15/Ravesli_15.cpp
+++ Ravesli_15/Ravesli_15.cpp
@@ -3,11 +3,57 @@
 
 #include <iostream>
 
-int main()
+// Lesson 19 function early declaration
+int add(int x, int y); // предварительное объявление функции add() (используется её прототип)
+
+void doMath(int a)
 {
-    std::cout << "Hello World!\n";
+    int b = 5;
+    std::cout << "doMath: a = " << a << " and b = " << b << std::endl;
+    a = 4;
+    std::cout << "doMath: a = " << a << " and b = " << b << std::endl;
 }
 
+int main()
+{
+    int a = 6;
+    int b = 7;
+    std::cout << "main: a = " << a << " and b = " << b << std::endl;
+    doMath(a);
+    std::cout << "main: a = " << a << " and b = " << b << std::endl;
+    
+    // Lesson 17
+    int a = 3;                      // a – это переменная, 3 – это литерал
+    std::cout << 5 + 2;             // 5 + 2 – это выражение, 5 и 2 – это литералы
+    std::cout << "Hello, world!";   // "Hello, world" – это тоже литерал
+
+    // Lesson 18
+    int nCost = 57;
+    int nPricePerItem = 24;
+    int nValue = 5;
+    int nNumberOfItems = 17;
+    std::cout << "Hello world!" << std::endl;                  // cout и endl находятся в библиотеке iostream
+    std::cout << "It is very nice to meet you!" << std::endl;  // эти комментарии более читабельны
+    std::cout << "Yeah!" << std::endl;                         // не так ли?
+    
+    // cout и endl находятся в библиотеке iostream
+    std::cout << "Hello world!" << std::endl;
+
+    // эти комментарии читать легче
+    std::cout << "It is very nice to meet you!" << std::endl;
+
+    // ведь они разделены дополнительными строками
+    std::cout << "Yeah!" << std::endl;
+
+    // Lesson 19
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << std::endl; // это работает, так как мы предварительно (выше функции main()) объявили функцию add()
+    return 0;
+}
+
+int add(int x, int y) // хотя определение функции add() находится ниже её вызова
+{
+    return x + y;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
